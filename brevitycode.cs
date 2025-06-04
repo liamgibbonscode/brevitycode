50417da66b921b2c40aedd3f130d059f9549f18b
liamgibbons <108643300+liamgibbonscode@users.noreply.github.com>
2025-06-04 17:36:35 +1000

    Add files via upload

---
+++ brevitycode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brevitycode : MonoBehaviour
+{
+    public GameObject whiteSquare;
+    public Camera cam;
+    public Color black;
+    public AudioClip blip;
+    AudioSource aS;
+
+    void Start()
+    {
+        cam.clearFlags = CameraClearFlags.SolidColor;
+        aS = whiteSquare.GetComponent<AudioSource>();
+    }
+
+    void OnMouseDown()
+    {
+        gameObject.SetActive(false);
+        cam.backgroundColor = black;
+        aS.PlayOneShot(blip, 1F);
+    }
+}
